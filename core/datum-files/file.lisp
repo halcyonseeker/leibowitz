a62bab9ffbe928600e742e281addaff3d524b3d4
ꙮ <ymir@ulthar.xyz>
2023-03-02 15:23:21 -0800

    Datum IDs are always stored as strings

--- core/datum-files/file.lisp
+++ core/datum-files/file.lisp
@@ -13,6 +13,9 @@
     ((f datum-file) &rest initargs &key &allow-other-keys)
   (declare (ignore initargs))
   (setf (datum-file-mime f) (datum-file-find-mime f))
+  ;; Datum IDs should always be strings, since in this case it's a
+  ;; file path it might be passed in as a pathname instead.
+  (setf (datum-id f) (namestring (datum-id f)))
   ;; I think we can implement file-type specific behavior here by
   ;; searching for a class named datum-file-major/minor and redefining
   ;; this instance's class to it.
