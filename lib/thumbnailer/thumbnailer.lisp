5cd958999be37f94ca4fc7748d9d1f2b159ff1c4
ꙮ <ymir@ulthar.xyz>
2023-10-19 14:29:32 -0700

    thumbnailer: replace assertion failure with more meaningful error for missing source file

--- lib/thumbnailer/thumbnailer.lisp
+++ lib/thumbnailer/thumbnailer.lisp
@@ -3,7 +3,8 @@
   (:use #:cl)
   (:export #:*thumbnail-cache-dir*
            #:get-thumbnail
-           #:unsupported-file-type))
+           #:unsupported-file-type
+           #:source-file-not-accessible))
 
 (in-package :thumbnailer)
 
@@ -22,7 +23,8 @@
 generating it if it doesn't exist or if PATH was modified after the
 thumbnail was last generated."
   (check-type path (or string pathname))
-  (assert (probe-file path))
+  (unless (probe-file path)
+    (error 'source-file-not-accessible :path path))
   (let* ((cached-path (get-cached-path path))
          (generate-thumbnail-p (or (not (probe-file cached-path))
                                    (> (file-write-date path)
@@ -41,6 +43,13 @@ thumbnail was last generated."
              (with-slots (mime path) c
                (format s "Unsupported mime type ~S for file ~S"
                        mime path)))))
+
+(define-condition source-file-not-accessible (error)
+  ((path :initarg :path))
+  (:report (lambda (c s)
+             (with-slots (path) c
+               (format s "Failed to create thumbnail for ~S, file doesn't exist."
+                       path)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Generators
