91ecd6109a456ff668d2bbc4b242f2a970609162
ꙮ <ymir@ulthar.xyz>
2023-10-24 14:07:06 -0700

    tests: cli helper macro should be very explicit about running main

--- tests/cli.lisp
+++ tests/cli.lisp
@@ -4,7 +4,8 @@
 
 (define-test cli)
 
-(defmacro define-cli-test (name command-line &body body)
+(defmacro define-cli-test (name (main) &body body)
+  (check-type main symbol)
   `(define-test ,name :parent cli
      (let (;; NOTE: each of these variables should be unbound at
            ;; startup, however capturing them like this binds them to
@@ -19,30 +20,32 @@
               (pathname
                (format NIL "/tmp/leibowitz_cli_test_cwd-tmp~36R/"
                        (random (expt 36 8)))))))
-       (unwind-protect
-            (progn
-              (main :test-harness-p T :test-argv ',command-line)
-              ,@body)
-         (uiop:delete-directory-tree *default-pathname-defaults* :validate T)))))
+       (labels ((,main (&rest cli)
+                  (leibowitz.cli:main :test-harness-p T :test-argv cli)))
+         (unwind-protect
+              ,@body
+           (uiop:delete-directory-tree *default-pathname-defaults* :validate T))))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Program context
 
-(define-cli-test infer-resource-files-without-root ("")
+(define-cli-test infer-resource-files-without-root (run)
+  (run)
   (is #'equal (uiop:xdg-data-home "leibowitz/") *data-directory*)
   (is #'equal (uiop:xdg-cache-home "leibowitz/") *cache-directory*)
   (is #'equal (user-homedir-pathname) *base-directory*)
   (of-type 'library *library*)
   (false *webserver*))
 
-(define-cli-test infer-resource-files-with-root ("-r" ".")
+(define-cli-test infer-resource-files-with-root (run)
+  (run "-r" ".")
   (is #'equal (merge-pathnames ".leibowitz/") *data-directory*)
   (is #'equal (merge-pathnames ".leibowitz/cache/") *cache-directory*)
   (is #'equal *default-pathname-defaults* *base-directory*)
   (of-type 'library *library*)
   (false *webserver*))
 
-(define-cli-test help-and-return-error-on-no-subcommand ()
+(define-cli-test help-and-return-error-on-no-subcommand (run)
   )
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
