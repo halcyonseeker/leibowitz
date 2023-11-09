c7bcb6d645d8639ce45ee436813c36a32e2e3977
ꙮ <ymir@ulthar.xyz>
2023-11-09 10:38:07 -0800

    tests: added mktmp helper function

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -9,3 +9,9 @@
        ,@(loop for var in tmpfiles
                collect `(delete-file ,var)))))
 
+(defun mktmp (&optional (prefix #P"/tmp/"))
+  "Create a temporary file in /tmp/, or under PREFIX if it's specified
+and return the pathname.  Unlike `with-tmp-files', the caller is
+responsible for cleanup."
+  (uiop:tmpize-pathname (merge-pathnames prefix "leibowitz_test_tmpfile")))
+
