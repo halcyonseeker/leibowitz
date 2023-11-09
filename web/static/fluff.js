b6f12b8b0b5947ff5de74e844fa47d784bb08eb6
ꙮ <ymir@ulthar.xyz>
2023-11-09 14:33:32 -0800

    web: js refactoring, notes, cleanup
    * Added links to notes on JS to header comment
    * $() as a shorthand for document.getElementbyid
    * Suppress type errors in console when tag-editor form isn't present

--- web/static/fluff.js
+++ web/static/fluff.js
@@ -1,22 +1,46 @@
-/* fluff.js — nonessential quality-of-life enhancements */
+/* fluff.js — nonessential quality-of-life enhancements
+ *
+ * NOTES
+ * =====
+ * * https://news.ycombinator.com/hn.js
+ * * Some bad advice, some good https://phuoc.ng/collection/html-dom/
+ *   * Discussion: https://news.ycombinator.com/item?id=38162435
+ * * How to no jquery https://youmightnotneedjquery.com/
+ * * How to vanilla js https://javascript.info/
+ */
+
+/***********************************************************************
+ * Helper Functions */
+
+function $(id) { return document.getElementById(id); }
+
+function replace_page_with(text)
+{
+	console.log("DBG: replace_page_with TEXT of type " + typeof(text));
+	document.open();
+	document.write(text);
+	document.close();
+}
+
+/***********************************************************************
+ * AJAX Form Submissions */
 
 function update_sidebar_or_error(req)
 {
 	if (req.readyState === XMLHttpRequest.DONE) {
-		if (req.status !== 200) {
-			/* Hopefully the backend sent an error page */
-			document.open();
-			document.write(req.responseText);
-			document.close();
-		} else {
-			document.getElementById("sidebar").innerHTML = req.responseText;
-		}
+		if (req.status !== 200)
+			/* Hopefully the backend sent an error page,
+			 * FIXME: add some frontend error handling
+			 * just in case it didn't */
+			replace_page_with(req.responseText);
+		else
+			$("sidebar").innerHTML = req.responseText;
 	}
 }
 
 function submit_tag_editor_asynchronously()
 {
-	const tag_data = document.getElementById("tag-editor-textarea").value;
+	const tag_data = $("tag-editor-textarea").value;
 	const req = new XMLHttpRequest();
 	/* Register a response handler */
 	req.onreadystatechange = () => { update_sidebar_or_error(req); };
@@ -27,13 +51,19 @@ function submit_tag_editor_asynchronously()
 	req.send(`tags=${encodeURIComponent(tag_data)}&ajax=t`);
 }
 
-/* Suppress default submit button behavior */
-document.getElementById("tag-editor-submit").setAttribute("type", "button");
-/* Handle S-RET; FIXME: avoid inserting a newline */
-document.getElementById("tag-editor-textarea").addEventListener("keydown", (e) => {
-	if (e.shiftKey && e.keyCode == 13) submit_tag_editor_asynchronously();
-});
-/* Handle press of the submit button asynchronously */
-document.getElementById("tag-editor-submit").addEventListener("click", (e) => {
-	submit_tag_editor_asynchronously();
-});
+if ($("tag-editor-submit")) {
+	/* Suppress default submit button behavior */
+	$("tag-editor-submit").setAttribute("type", "button");
+	/* Handle press of the submit button asynchronously */
+	$("tag-editor-submit").addEventListener("click", (e) => {
+		submit_tag_editor_asynchronously();
+	});
+}
+
+if ($("tag-editor-textarea")) {
+	/* Handle S-RET; FIXME: avoid inserting a newline */
+	$("tag-editor-textarea").addEventListener("keydown", (e) => {
+		if (e.shiftKey && e.keyCode == 13)
+			submit_tag_editor_asynchronously();
+	});
+}
