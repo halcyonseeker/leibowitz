cf574495a27313f8fa8427dcdaa10d3a81ee38bb
ꙮ <ymir@ulthar.xyz>
2023-12-07 11:11:02 -0800

    tests: mktmp can create directories as well as files

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -9,9 +9,16 @@
        ,@(loop for var in tmpfiles
                collect `(delete-file ,var)))))
 
-(defun mktmp (&optional (prefix #P"/tmp/"))
+(defun mktmp (&optional (prefix #P"/tmp/") (type :file))
   "Create a temporary file in /tmp/, or under PREFIX if it's specified
 and return the pathname.  Unlike `with-tmp-files', the caller is
 responsible for cleanup."
-  (uiop:tmpize-pathname (merge-pathnames prefix "leibowitz_test_tmpfile")))
+  (assert (member type '(:file :dir)))
+  (case type
+    (:file (uiop:tmpize-pathname
+            (merge-pathnames prefix "leibowitz_test_tmpfile")))
+    (:dir (ensure-directories-exist
+           (merge-pathnames
+            (format NIL "leibowitz_test_tmpdir-~A/" (random (expt 36 8)))
+            prefix)))))
 
