1dc9c2afcf0e923e2c778a9885eb2af335d22298
ꙮ <ymir@ulthar.xyz>
2024-04-04 18:02:06 -0700

    tests: utility function mktmp can now also create fifos

--- tests/utils.lisp
+++ tests/utils.lisp
@@ -13,12 +13,14 @@
   "Create a temporary file in /tmp/, or under PREFIX if it's specified
 and return the pathname.  Unlike `with-tmp-files', the caller is
 responsible for cleanup."
-  (assert (member type '(:file :dir)))
-  (case type
-    (:file (uiop:tmpize-pathname
-            (merge-pathnames prefix "leibowitz_test_tmpfile")))
-    (:dir (ensure-directories-exist
-           (merge-pathnames
-            (format NIL "leibowitz_test_tmpdir-~A/" (random (expt 36 8)))
-            prefix)))))
+  (assert (member type '(:file :dir :fifo)))
+  (let ((path (merge-pathnames
+               (format NIL "leibowitz_test_tmp~A-~A"
+                       (string-downcase type) (random (expt 36 8)))
+               prefix)))
+    (case type
+      (:file (osicat-posix:creat path osicat-posix:*default-open-mode*))
+      (:dir  (ensure-directories-exist (format NIL "~A/" path)))
+      (:fifo (osicat-posix:mkfifo path osicat-posix:*default-open-mode*)))
+    path))
 
