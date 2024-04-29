120f5a372a24b76123c6346e6b76deab31b1de7a
ꙮ <ymir@ulthar.xyz>
2024-04-29 15:52:35 -0700

    core: homedir collection-applicable-p now handles non-existant files
    The stuff with uiop:native-namestring and uiop:parse-unix-namestring
    will be fully explained in a forthcoming commit, this refactoring is a
    small part of my attempt at making the library's pathname handling
    less brittle.

--- core/collections/homedir.lisp
+++ core/collections/homedir.lisp
@@ -26,14 +26,27 @@
   (print-unreadable-object (c stream :type T)
     (format stream "~A" (collection-homedir-root c))))
 
-(defmethod collection-applicable-p ((c collection-homedir) id)
-  (check-type id (or string pathname))
-  (let ((root (namestring (collection-homedir-root c)))
-        ;; Handle the case where some part of our root path is a
-        ;; symlink, like how FreeBSD links /home to /usr/home
-        (root-without-symlinks (namestring (truename (collection-homedir-root c))))
-        ;; If the file exists on disk, resolve symlinks
-        (id (namestring (if (probe-file id) (truename id) id))))
+;;; FIXME: Since `uiop:parse-unix-namestring' can convert URLs to
+;;; pathnames just fine and since we're attempting to handle
+;;; non-existent paths too this implementation returns T for URLs,
+;;; causing all of the `library-get-datum-collection' tests to fail.
+;;; This is okay since I think the collections API is something of a
+;;; misfeature and I'll probably rip it out soon.
+(defmethod collection-applicable-p ((c collection-homedir) (id string))
+  (let* ((id (uiop:parse-unix-namestring id))
+         (root (uiop:native-namestring (collection-homedir-root c)))
+         ;; Handle the case where some part of our root path is a
+         ;; symlink, like how FreeBSD links /home to /usr/home
+         (root-without-symlinks
+           (uiop:native-namestring (truename (collection-homedir-root c))))
+         ;; If the file exists on disk, resolve symlinks
+         (id (uiop:native-namestring (if (probe-file id)
+                                         (truename id)
+                                         (merge-pathnames id)))))
+    ;; FIXME: As a result of the way we're processing ID through
+    ;; uiop:parse-unix-namestring → merge-pathnames →
+    ;; uiop:native-namestring this will return T for ANY value of ID
+    ;; that doesn't point to an existing file :/
     (if (or (eql 0 (search root id))
             (eql 0 (search root-without-symlinks id)))
         c
